1e681da92ecd4f070b681a14e0a5f053a60c5aa2
Jesper Tennevik <jesper.tennevik@gmail.com>
2026-04-09 15:14:40 +0200

    Added: user input for ServerListener port

--- src/Server/ServerListener.java
+++ src/Server/ServerListener.java
@@ -1,12 +1,25 @@
 package Server;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerListener {
+    static int port;
+
     static void main(String[] args){
-        try(ServerSocket serverSocket = new ServerSocket(12345)){
+
+        boolean validInput = false;
+        while(!validInput){
+            String portStr = JOptionPane.showInputDialog(null, "Port:");
+            if(portStr == null){ return; }
+
+            if(isValidPort(portStr)) { validInput = true; }
+            else{ JOptionPane.showMessageDialog(null, "Invalid port"); }
+        }
+
+        try(ServerSocket serverSocket = new ServerSocket(port)){
             while(true){
                Socket socket = serverSocket.accept();
                new Server(socket);
@@ -14,5 +27,12 @@ public class ServerListener {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    static private boolean isValidPort(String portStr) {
+        try {
+            port = Integer.parseInt(portStr);
+            return port >= 1 && port <= 65535;
+        } catch (NumberFormatException e) { return false; }
     }
 }
